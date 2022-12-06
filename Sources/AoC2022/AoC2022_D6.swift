d1bc2ea5a6be5dd045d9fbf3b2bec94c30cc6bc7
KieranConlon <kieran.conlon@gmail.com>
2022-12-06 16:10:35 +0000

    Refactored for cleaner code on D6

--- Sources/AoC2022/AoC2022_D6.swift
+++ Sources/AoC2022/AoC2022_D6.swift
@@ -23,25 +23,18 @@ public func AoC2022_D6(inputFile: String) -> (puzzle1: Int, puzzle2: Int) {
   var startOfPacketCount = startOfPacketMarkerLen - 1
   var startOfMessageCount = startOfMessageMarkerLen - 1
   
-  for i in 0..<rawInput.count {
-    let from = i
-    let toEndOfMarker = from + startOfPacketMarkerLen
-    let toEndOfMessage = from + startOfMessageMarkerLen
-    
-    let start = rawInput.index(rawInput.startIndex, offsetBy: from, limitedBy: rawInput.endIndex) ?? rawInput.endIndex
-    let endMarker = rawInput.index(rawInput.startIndex, offsetBy: toEndOfMarker, limitedBy: rawInput.endIndex) ?? rawInput.endIndex
-    let endMessage = rawInput.index(rawInput.startIndex, offsetBy: toEndOfMessage, limitedBy: rawInput.endIndex) ?? rawInput.endIndex
-    
-    let marker = Set(rawInput[start..<endMarker])
-    let message = Set(rawInput[start..<endMessage])
-    
+  for from in 0..<rawInput.count {
+    let start =       rawInput.index(rawInput.startIndex, offsetBy: from,                    limitedBy: rawInput.endIndex)!
+    let endMarker =   rawInput.index(start,               offsetBy: startOfPacketMarkerLen,  limitedBy: rawInput.endIndex)!
+    let endMessage =  rawInput.index(start,               offsetBy: startOfMessageMarkerLen, limitedBy: rawInput.endIndex)!
+
     if !markerFound {
-      markerFound = (marker.count == startOfPacketMarkerLen)
+      markerFound = (Set(rawInput[start..<endMarker]).count == startOfPacketMarkerLen)
       startOfPacketCount += 1
     }
     
     if !messageFound {
-      messageFound = (message.count == startOfMessageMarkerLen)
+      messageFound = (Set(rawInput[start..<endMessage]).count == startOfMessageMarkerLen)
       startOfMessageCount += 1
     }
     
